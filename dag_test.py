e6b06fded0bb69a55d1fce828c83852d08f680dc
suhwan <suhwan1124@naver.com>
2024-11-14 18:22:08 +0900

    파케이 테스트

--- dag_test.py
+++ dag_test.py
@@ -5,35 +5,39 @@ from datetime import datetime
 import pyarrow as pa
 import io
 import pandas as pd
+import logging
 
 
 
 # RDS 연결 테스트 함수
 def test_rds_connection():
     try:
-        # PostgresHook을 사용해 데이터베이스에 연결
+        # RDS 연결 코드
         pg_hook = PostgresHook(postgres_conn_id='my_postgres_conn')
         conn = pg_hook.get_conn()
         cursor = conn.cursor()
-        # 간단한 쿼리 실행
         cursor.execute("SELECT 1;")
         result = cursor.fetchone()
-        print(f"RDS 연결 테스트 결과: {result}")
+        logging.info(f"RDS 연결 테스트 결과: {result}")
+        
         # 샘플 데이터프레임 생성
         df = pd.DataFrame({
             'column1': [1, 2, 3],
             'column2': ['a', 'b', 'c']
         })
 
-        # Parquet 형식으로 변환 테스트
-        buffer = io.BytesIO()
-        df.to_parquet(buffer, index=False, engine='pyarrow')
-        print("Parquet 변환 성공")
+        # Parquet 변환 코드
+        try:
+            buffer = io.BytesIO()
+            df.to_parquet(buffer, index=False, engine='pyarrow')
+            logging.info("Parquet 변환 성공")
+        except Exception as e:
+            logging.error(f"Parquet 변환 실패: {e}")
         
         cursor.close()
         conn.close()
     except Exception as e:
-        print(f"RDS 연결 실패: {e}")
+        logging.error(f"RDS 연결 실패: {e}")
 
 # Airflow DAG 정의
 with DAG(
